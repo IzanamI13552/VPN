fd8a6faf7442c021e35a4a97226df44f23fb7978
kille <Safo13552@gmail.com>
2025-11-23 13:50:26 +0300

    add adapter

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"golang.zx2c4.com/wintun"
+	"log"
+)
 
+func main() {
+	adapter, err := wintun.CreateAdapter("MyWintun", "MyWintun", nil)
+	if err != nil {
+		log.Fatalf("Ошибка создание адаптера : %v", err)
+	}
+	log.Printf("Адаптер создан : %v", adapter)
 }
