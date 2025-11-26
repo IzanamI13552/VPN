6bf4973a978d9901e5f9eb2e0e1c4f5a1a93a6a9
Daniel <safo13552@gmail.com>
2025-11-26 18:02:30 +0300

    add (key, UDP, Receiving packages)

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module VPN
 
 go 1.24.3
-
-require golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2
-
-require golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
--- main.go
+++ main.go
@@ -1,19 +1,65 @@
 package main
 
 import (
-	"golang.zx2c4.com/wintun"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
 	"log"
+	"net"
 )
 
+const serverAddr = "127.0.0.1:8080"
+
 func main() {
-	adapter, err := wintun.CreateAdapter("MyWintun", "MyWintun", nil)
+	// Создание сикретного ключа с помощью rand
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Создание UDP-адреса
+	addr, _ := net.ResolveUDPAddr("udp", serverAddr)
+	// Запуск UDP сервера
+	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		log.Fatalf("Ошибка создание адаптера : %v", err)
 	}
-	log.Printf("Адаптер создан : %v", adapter)
-	miltiplay(5)
-}
+	defer conn.Close()
 
-func miltiplay(num int) int {
-	return num * num
+	fmt.Println("VPN слушаю сервер ", serverAddr)
+	// Создаем буфер для вход данных
+	buf := make([]byte, 2048)
+	// Прием пакетов
+	for {
+		n, clientAddr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			continue
+		}
+		nonceSize := aead.NonceSize()
+
+		if n < nonceSize {
+			continue
+		}
+		nonce := buf[:12]
+		ciphertext := buf[12:n]
+		plain, err := aead.Open(nil, nonce, ciphertext, nil)
+		if err != nil {
+			fmt.Println("Ошибка расшифровки")
+			continue
+		}
+		// Вывод результата
+		fmt.Printf("От %s: %s\n", clientAddr, string(plain))
+
+	}
 }
